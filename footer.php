3c734658781e77b2b223b74ce60f46740b0c70a2
Abhi-1U <62772223+Abhi-1U@users.noreply.github.com>
2023-08-29 01:46:08 +0530

    website without video

---
+++ footer.php
@@ -0,0 +1,82 @@
+<footer id="gtco-subscribe" role="contentinfo">
+		<div class="gtco-container">
+			<div class="row row-p	b-md">
+
+				<div class="col-md-4">
+					<div class="gtco-widget">
+						<h3 style="color: white;">About Us</h3>
+						<p>Kraftuz is a Service provider for end-to-end blend of variety of Live Events. Crafting like craftsman in marketing experiences that transcend expectations & ignite the imagination. We redefine the event management landscape by offering a fresh, innovative, and strategic approach to live event planning and execution. With a passion for creativity and a commitment to excellence, we specialize in curating unforgettable experiences that leave lasting impressions.</p>
+					</div>
+				</div>
+
+				<div class="col-md-4 col-md-push-1">
+					<div class="gtco-widget">
+						<h3 style="color: white;">Links</h3>
+						<ul class="gtco-footer-links">
+							<li><a href="#">Knowledge Base</a></li>
+							<li><a href="#">Career</a></li>
+							<li><a href="#">Press</a></li>
+							<li><a href="#">Terms of services</a></li>
+							<li><a href="#">Privacy Policy</a></li>
+						</ul>
+					</div>
+				</div>
+
+				<div class="col-md-4">
+					<div class="gtco-widget">
+						<h3 style="color: white;">Get In Touch</h3>
+						<ul class="gtco-quick-contact">
+							<li><a href="#"><i class="icon-phone"></i> 022 2392 0942</a></li>
+							<li><a href="#"><i class="icon-mail2"></i> kraftuzsket2023@gmail.com</a></li>
+							<li><a href="#"><i class="icon-chat"></i> Live Chat</a></li>
+						</ul>
+					</div>
+				</div>
+
+			</div>
+
+			<div class="row copyright">
+				<div class="col-md-12">
+					<p class="pull-left">
+						<small class="block">&copy; All Rights Reserved.</small> 
+						<small class="block">Designed by <a href="http://gettemplates.co/" target="_blank">Mayur Shirsekar</a> </small>
+					</p>
+					<p class="pull-right">
+						<ul class="gtco-social-icons pull-right">
+							<li><a href="#"><i class="icon-instagram"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-linkedin"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+						</ul>
+					</p>
+				</div>
+			</div>
+
+		</div>
+	</footer>
+	</div>
+
+
+<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- countTo -->
+	<script src="js/jquery.countTo.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,93 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Kraftuz</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="Kraftuz team" />
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- <link href="https://fonts.googleapis.com/css?family=Droid+Sans" rel="stylesheet"> -->
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Themify Icons-->
+	<link rel="stylesheet" href="css/themify-icons.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+    <body>
+    <div class="gtco-loader"></div>
+	
+    <div id="page">
+	<nav class="gtco-nav" role="navigation">
+		<div class="gtco-container">
+		<div class="col-sm-2 col-xs-4">
+					<div id="gtco-logo"><a href="index.php"><img src="images/logo.png"  width="120%" ></a></div>
+				</div>
+		
+				<div class="col-md-10 text-right gtco-contact">
+					<ul >
+						<li class="active"><a href="#"><i class="ti-mobile"></i> 022 2392 0942 </a></li>
+						
+						<li><a href="mailto:kraftuzsket2023@gmail.com"><i class="icon-mail2"></i></a></li>
+						<li><a href="#"><i class="ti-facebook"></i></a></li>
+						<li><a href="https://instagram.com/kraftuzsket"><i class="ti-instagram"></i></a></li>
+					</ul>
+				</div>
+
+	
+				
+				<div class="col-xs-15 text-right menu-1 gtco-contact">
+					<ul class="">
+					
+						<li ><a href="index.php" >Home</a></li>
+						<li ><a href="about.php">About</a></li>
+						<li  ><a href="services.php">Events</a></li>
+						<li  ><a href="portfolio.php">Portfolio</a></li>
+						<li  ><a href="csr.php">CSR</a></li>
+						<li><a href="contact.php">Contact</a></li>
+						
+					</ul>
+				</div>
+		
+			
+		</div>
+	</nav>
